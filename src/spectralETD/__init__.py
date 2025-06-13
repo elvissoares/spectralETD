6f22212d771ef8d0e79c0faf3cc2732eb63e9722
Elvis Soares <elvis.asoares@gmail.com>
2025-06-13 17:03:10 -0300

    Updating __init__.py

--- src/spectralETD/__init__.py
+++ src/spectralETD/__init__.py
@@ -1,1 +1,13 @@
-from . import spectralETD, volumerender
+# src/spectralETD/__init__.py
+
+"""
+spectraletd: a Python library which combines Exponential Time Differencing and Pseudo-spectral Methods for Phase-Field Model Equation in three dimensions in a GPU-accelerated framework.
+"""
+
+__author__  = "Elvis Soares <elvis.asoares@gmail.com>"
+__license__ = "MIT"
+
+from .spectralETD import SpectralETD
+from .volumerender import volumerender
+
+__all__ = ["SpectralETD","volumerender"]
